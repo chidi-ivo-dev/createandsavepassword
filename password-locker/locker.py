1e447c5c2e97aca1658ff75dea650b4efb85da29
danalvin <mungaiwaituika@gmail.com>
2018-08-25 12:17:25 +0300

    class added

--- password-locker/locker.py
+++ password-locker/locker.py
@@ -120,3 +120,21 @@ def login():
                     login()
 
                 return False
+
+
+class User:
+
+    """
+    Class that generates new instances of Users
+    """
+    User_list = []
+
+    def __init__(self,username,password,email):
+
+        """
+        init method that defines properties for our objects
+        """
+
+        self.username = username
+        self.password = password
+        self.email = email
