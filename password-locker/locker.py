2f94a53cd0feb86d480e614c46f9fb8637ba443e
danalvin <mungaiwaituika@gmail.com>
2018-08-25 12:11:44 +0300

    more commands added

--- password-locker/locker.py
+++ password-locker/locker.py
@@ -1,8 +1,10 @@
 import random
 import pyperclip
+
+
 def all():
     print('Already have an account? (y/n)')
-    answer =input().lower()
+    answer = input().lower()
     if answer == 'n':
         print("enter your name:")
         username = input()
@@ -14,7 +16,7 @@ def all():
         email = input()
         print('/n')
 
-        handle = open("details.txt","a")
+        handle = open("details.txt", "a")
 
         handle.write(username)
         handle.write(" ")
@@ -24,7 +26,8 @@ def all():
         handle.write("/n")
 
         handle.close()
-        print("Account you just made has been created successfully /n Would you like to do more with password locker?(y/n)")
+        print("Account you just made has been created successfully /n Would you like to do more with password "
+              "locker?(y/n)")
         responce = input().lower()
         if responce == 'y':
             generate_account()
@@ -32,13 +35,36 @@ def all():
         else:
             print("be sure to come back if you'd like to save a password, Have a good day!")
 
-    elif answer =='y':
+    elif answer == 'y':
         login()
-
 
 
 def generate_account():
     print("Which account would you like to generate an account for?")
-    account_to_be_saved =input()
+    account_to_be_saved = input()
     print("Enter 'auto'- for an auto-generated password or 'type'- to type in your own password")
+    choice = input()
+    if choice == 'auto':
+        reversed_account_name = account_to_be_saved[::-1]
+        r = str(random.randint(0, 20))
+        password = reversed_account_name + r
+        print(f"your new password is {password}")
+        handle.write(account_to_be_saved)
+        handle.write(" ")
+        handle.write(password)
+        handle.write("")
 
+        handle.close()
+
+        print(f"the password to your new account {account_to_be_saved} account is {password} and has been saved to "
+              f"your clipboard has been saved and can be copied to the clipboard, you couls even apste it here in the "
+              f"terminal. try it, (ctr + shift + v)")
+        pyperclip.copy(password)
+
+    else:
+        print("type in your password")
+        password = input()
+        pyperclip.copy(password)
+        print(f"the password to your new account {account_to_be_saved} account is {password} and has been saved to "
+              f"your clipboard has been saved and can be copied to the clipboard, you couls even paste it here in the "
+              f"terminal. try it, (ctr + shift + v)")
